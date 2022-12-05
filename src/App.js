198ab4e4924f8db48fd475ba9cfc07cffd81929a
bentseng163 <bentseng163@gmail.com>
2022-12-05 02:19:44 -0500

    update questionSet state complete

--- src/App.js
+++ src/App.js
@@ -9,63 +9,99 @@ let globalItemCount = 0;
 function App() {
 
 
+
   const [listItems, setListItems] = useState([
     { name: "Question1", type: "", id: 0 }
   ]);
 
-  const [questionName, setQuestionName] = useState("");
-  const [description, setDescription] = useState("");
-  const [optionA, setOptionA] = useState("");
-  const [optionB, setOptionB] = useState("");
-  const [optionC, setOptionC] = useState("");
-  const [optionD, setOptionD] = useState("");
-  const [answer, setAnswer] = useState("");
-  const [hint, setHint] = useState("");
-  const [feedback, setFeedback] = useState("");
+  const [questionSet, setQuestionSet] = useState([]);
+
+  const [currentQuestion, setCurrentQuestion] = useState({});
+
+  function updateCurrentQuestion(question){
+    setCurrentQuestion(question)
+
+    let tempSet = [...questionSet];
+
+
+    var questionIndex = tempSet.findIndex((q) => q.id === question.id)
+    console.log(questionIndex)
+    if (questionIndex == -1){
+      tempSet.push(question)
+    } else {
+      tempSet[questionIndex] = question
+    }
+    console.log(tempSet)
+    setQuestionSet(tempSet)
 
 
 
-  const [currentQuestion, setCurrentQuestion] = useState({})
+    }
 
-  function updateQuestionName(e) {
-    setQuestionName(e.currentTarget.value);
-  }
+  
 
-  function updateDescription(e) {
-    setDescription(e.currentTarget.value);
-  }
+  
 
-  function updateOptionA(e) {
-    setOptionA(e.currentTarget.value);
-  }
 
-  function updateOptionB(e) {
-    setOptionB(e.currentTarget.value);
-  }
 
-  function updateOptionC(e) {
-    setOptionC(e.currentTarget.value);
-  }
 
-  function updateOptionD(e) {
-    setOptionD(e.currentTarget.value);
-  }
 
-  function updateAnswer(e) {
-    setAnswer(e.currentTarget.value);
-  }
 
-  function updateHint(e) {
-    setHint(e.currentTarget.value);
-  }
 
-  function updateFeedback(e) {
-    setFeedback(e.currentTarget.value);
-  }
 
-  function saveQuestion(id){
-    setCurrentQuestion({ name: questionName, description: description, optionA: optionA, optionB: optionB, optionC: optionC, optionD: optionD, answer: answer, hint: hint,feedback: feedback })
-  }
+  // const [questionName, setQuestionName] = useState("");
+  // const [description, setDescription] = useState("");
+  // const [optionA, setOptionA] = useState("");
+  // const [optionB, setOptionB] = useState("");
+  // const [optionC, setOptionC] = useState("");
+  // const [optionD, setOptionD] = useState("");
+  // const [answer, setAnswer] = useState("");
+  // const [hint, setHint] = useState("");
+  // const [feedback, setFeedback] = useState("");
+
+
+
+  // const [currentQuestion, setCurrentQuestion] = useState({})
+
+  // function updateQuestionName(e) {
+  //   setQuestionName(e.currentTarget.value);
+  // }
+
+  // function updateDescription(e) {
+  //   setDescription(e.currentTarget.value);
+  // }
+
+  // function updateOptionA(e) {
+  //   setOptionA(e.currentTarget.value);
+  // }
+
+  // function updateOptionB(e) {
+  //   setOptionB(e.currentTarget.value);
+  // }
+
+  // function updateOptionC(e) {
+  //   setOptionC(e.currentTarget.value);
+  // }
+
+  // function updateOptionD(e) {
+  //   setOptionD(e.currentTarget.value);
+  // }
+
+  // function updateAnswer(e) {
+  //   setAnswer(e.currentTarget.value);
+  // }
+
+  // function updateHint(e) {
+  //   setHint(e.currentTarget.value);
+  // }
+
+  // function updateFeedback(e) {
+  //   setFeedback(e.currentTarget.value);
+  // }
+
+  // function saveQuestion(id){
+  //   setCurrentQuestion({ name: questionName, description: description, optionA: optionA, optionB: optionB, optionC: optionC, optionD: optionD, answer: answer, hint: hint,feedback: feedback })
+  // }
 
 
 
@@ -82,7 +118,7 @@ function App() {
     globalItemCount++;
     setListItems([
       ...listItems,
-      { name: questionName, type: "", id: globalItemCount }
+      { name: "", type: "", id: globalItemCount }
     ]);
   }
 
@@ -98,23 +134,24 @@ function App() {
   const questionSetComponents = listItems.map((item) => {
     return (
       <ListItem
-        text={item.name}
+        // text={item.name}
         id={item.id}
         key={item.id}
         deleteItem={deleteItem}
-        updateQuestionName={updateQuestionName}
-        updateDescription={updateDescription}
+        updateCurrentQuestion={updateCurrentQuestion}
+        // updateQuestionName={updateQuestionName}
+        // updateDescription={updateDescription}
 
-        updateOptionA={updateOptionA}
-        updateOptionB={updateOptionB}
-        updateOptionC={updateOptionC}
-        updateOptionD={updateOptionD}
+        // updateOptionA={updateOptionA}
+        // updateOptionB={updateOptionB}
+        // updateOptionC={updateOptionC}
+        // updateOptionD={updateOptionD}
 
-        updateAnswer={updateAnswer}
-        updateHint={updateHint}
-        updateFeedback={updateFeedback}
+        // updateAnswer={updateAnswer}
+        // updateHint={updateHint}
+        // updateFeedback={updateFeedback}
 
-        saveQuestion={saveQuestion}
+        // saveQuestion={saveQuestion}
 
         
 
--- src/ListItem.js
+++ src/ListItem.js
@@ -1,6 +1,65 @@
+import { useState } from "react";
 import { FaTrash } from "react-icons/fa";
 
 export default function ListItem(props) {
+
+
+  const [questionName, setQuestionName] = useState("");
+  const [description, setDescription] = useState("");
+  const [optionA, setOptionA] = useState("");
+  const [optionB, setOptionB] = useState("");
+  const [optionC, setOptionC] = useState("");
+  const [optionD, setOptionD] = useState("");
+  const [answer, setAnswer] = useState("");
+  const [hint, setHint] = useState("");
+  const [feedback, setFeedback] = useState("");
+  const [questionInfo, setQuestionInfo] = useState({});
+
+
+
+
+  function updateQuestionName(e) {
+    setQuestionName(e.currentTarget.value);
+  }
+
+  function updateDescription(e) {
+    setDescription(e.currentTarget.value);
+  }
+
+  function updateOptionA(e) {
+    setOptionA(e.currentTarget.value);
+  }
+
+  function updateOptionB(e) {
+    setOptionB(e.currentTarget.value);
+  }
+
+  function updateOptionC(e) {
+    setOptionC(e.currentTarget.value);
+  }
+
+  function updateOptionD(e) {
+    setOptionD(e.currentTarget.value);
+  }
+
+  function updateAnswer(e) {
+    setAnswer(e.currentTarget.value);
+  }
+
+  function updateHint(e) {
+    setHint(e.currentTarget.value);
+  }
+
+  function updateFeedback(e) {
+    setFeedback(e.currentTarget.value);
+  }
+
+  function saveQuestion(){
+    const questionInfo = { id: props.id, name: questionName, description: description, optionA: optionA, optionB: optionB, optionC: optionC, optionD: optionD, answer: answer, hint: hint,feedback: feedback }
+    props.updateCurrentQuestion(questionInfo)
+  }
+
+
   return (
     <div
     //   style={{
@@ -10,49 +69,49 @@ export default function ListItem(props) {
     //   }}
     >
     <p>Question Name:</p>
-    <input onChange={props.updateQuestionName} type="text"></input>
+    <input onChange={updateQuestionName} type="text"></input>
 
     <p>Description:</p>
-    <textarea rows="10" cols="80" onChange={props.updateDescription} type="text"></textarea>
+    <textarea rows="10" cols="80" onChange={updateDescription} type="text"></textarea>
     <div>  
         <p>Options</p> 
 
         <div className="option">
         <p>Option A</p>
-        <input onChange={props.updateOptionA} type="text"></input>
+        <input onChange={updateOptionA} type="text"></input>
         </div> 
 
         <div className="option">
         <p>Option B</p>
-        <input onChange={props.updateOptionB} type="text"></input>
+        <input onChange={updateOptionB} type="text"></input>
         </div>
 
         <div className="option">
         <p>Option C</p>
-        <input onChange={props.updateOptionC} type="text"></input>
+        <input onChange={updateOptionC} type="text"></input>
         </div> 
 
         <div className="option">
         <p>Option D</p>
-        <input onChange={props.updateOptionD} type="text"></input>
+        <input onChange={updateOptionD} type="text"></input>
         </div> 
     
     </div> 
 
     <div>
         <p>Answer</p>
-        <input onChange={props.updateAnswer} type="text"></input>
+        <input onChange={updateAnswer} type="text"></input>
     </div>
 
 
     <div>
         <p>Hint</p>
-        <textarea rows="5" cols="80" onChange={props.updateHint} type="text"></textarea>
+        <textarea rows="5" cols="80" onChange={updateHint} type="text"></textarea>
     </div>
 
     <div>
         <p>Explanatory Feedback</p>
-        <textarea rows="5" cols="80" onChange={props.updateFeedback} type="text"></textarea>
+        <textarea rows="5" cols="80" onChange={updateFeedback} type="text"></textarea>
     </div>
 
 
@@ -68,7 +127,7 @@ export default function ListItem(props) {
 
       {/* <p style={{ marginLeft: "10px" }}>{props.text}</p> */}
 
-      <button onClick={props.saveQuestion(props.id)} >Save</button>
+      <button onClick={saveQuestion} >Save</button>
       <button >Edit</button>
 
 
