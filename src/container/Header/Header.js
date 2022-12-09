6d2339774a385134a00213a9cc9743075cdbda2c
Phoebe <hsuanju.wang@gmail.com>
2022-12-08 22:35:46 -0500

    Updated new header

--- src/container/Header/Header.js
+++ src/container/Header/Header.js
@@ -7,15 +7,15 @@ export default function Header(props){
             <h2>Perspective</h2>
             { props.currentUserStatus === 'teacher' && props.currentUser !== undefined &&
                 <div className="navigation">
-                    <Link className='navLink' to="/">Editor</Link>
+                    <Link className='navLink' to="/">Dashboard</Link>
                     {/* <Link className='navLink' to="/preview/"> Preview </Link> */}
                     <Link className='navLink' to="/login/"> LogOut </Link> 
                 </div>
             }
             { props.currentUserStatus === 'student' && props.currentUser !== undefined &&
                 <div className="navigation">
-                    <Link className='navLink' to="/">Home</Link>
-                    <Link className='navLink' to="/Result/"> Result</Link>
+                    <Link className='navLink' to="/">Dashboard</Link>
+                    <Link className='navLink' to="/Result/"> Record</Link>
                     <Link className='navLink' to="/login/"> LogOut </Link> 
                 </div>
             }
